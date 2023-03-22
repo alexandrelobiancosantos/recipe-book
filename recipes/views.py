a1764d087a588b436d29117903e7beab9ecfd94a
Alexandre Lo Bianco <brewnext@gmail.com>
2023-03-22 18:05:02 -0300

    first commit

---
+++ recipes/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+from utils.recipes.factory import make_recipe
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(7)],
+    })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+    })
