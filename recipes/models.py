6c311773012e2fa49e9c18e9d0260cf7c8419def
Alexandre Lo Bianco <brewnext@gmail.com>
2023-03-22 19:57:28 -0300

    adin e models

--- recipes/models.py
+++ recipes/models.py
@@ -5,6 +5,8 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=65)
+    def __str__(self):
+        return self.name
 
 
 class Recipe(models.Model):
