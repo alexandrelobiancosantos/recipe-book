b98c2d832a699706986b82cfd318229a8c77890b
Alexandre Lo Bianco <brewnext@gmail.com>
2023-03-26 14:20:02 -0300

    separando testes de url e drenderização, criada pasta tets

--- recipes/tests/test_views.py
+++ recipes/tests/test_views.py
@@ -3,23 +3,9 @@ from django.urls import resolve, reverse
 
 from recipes import views
 
-
-# URL tests
-class RecipeURLsTest(TestCase):
-    def test_recipe_home_url_is_correct(self):
-        home_url = reverse('recipes:home')
-        self.assertEqual(home_url, '/')
-
-    def test_recipe_category_url_is_correct(self):
-        category_url = reverse('recipes:category', kwargs={'category_id': 1})
-        self.assertEqual(category_url, '/recipes/category/1/')
-
-    def test_recipe_details_url_is_correct(self):
-        recipe_url = reverse('recipes:recipe', kwargs={'id': 1})
-        self.assertEqual(recipe_url, '/recipes/1/')
-
-
 # Render tests
+
+
 class RecipeViewsTest(TestCase):
     def test_recipe_home_views_function_is_correct(self):
         view_home = resolve('/')
